36709aa07daf0270e98d07728e75cd5a78250146
shtayerc <david.murko@mailbox.org>
2020-03-17 10:35:25 +0100

    Fix tig config filename

--- symlink.sh
+++ symlink.sh
@@ -6,5 +6,5 @@ mkdir -p "$HOME"/.config/fish
 DIR="$(realpath "$(dirname "$0")")"
 ln -sf "$DIR"/tmux.conf "$HOME"/.tmux.conf
 ln -sf "$DIR"/init.vim  "$HOME"/.config/nvim/init.vim
-ln -sf "$DIR"/tigrc "$HOME"/.config/tig/tigrc
+ln -sf "$DIR"/tigrc "$HOME"/.config/tig/config
 ln -sf "$DIR"/config.fish "$HOME"/.config/fish/config.fish
