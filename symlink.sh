92ac5b5d56c8f13c58b9d4abcea0003c67bac992
shtayerc <david.murko@mailbox.org>
2020-04-29 16:40:59 +0200

    tmux.conf moved into .config/tmux

--- symlink.sh
+++ symlink.sh
@@ -2,9 +2,10 @@
 mkdir -p "$HOME"/.config/nvim
 mkdir -p "$HOME"/.config/tig
 mkdir -p "$HOME"/.config/fish
+mkdir -p "$HOME"/.config/tmux
 
 DIR="$(realpath "$(dirname "$0")")"
-ln -sf "$DIR"/tmux.conf "$HOME"/.tmux.conf
+ln -sf "$DIR"/tmux.conf "$HOME"/.config/tmux/tmux.conf
 ln -sf "$DIR"/init.vim  "$HOME"/.config/nvim/init.vim
 ln -sf "$DIR"/tigrc "$HOME"/.config/tig/config
 ln -sf "$DIR"/config.fish "$HOME"/.config/fish/config.fish
